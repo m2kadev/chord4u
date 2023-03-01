5db06a1f8ae26471445d84762938608e357614a8
Clickr <hiclickr@Hiclickrs-MacBook-Pro.local>
2023-03-02 02:18:07 +0630

    add lyric background color

--- src/components/partials/ShowChordOverlay.js
+++ src/components/partials/ShowChordOverlay.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ShowChordOverlay = ({currentLyric, setShowOverlay}) => {
   return (
-    <div className='show-chord-overlay'>
+    <div className='show-chord-overlay' style={{background: currentLyric?.acf?.thumbnail_background_color}}>
         <button onClick={() => setShowOverlay(false)}>X</button>
         <div className="show-chord" dangerouslySetInnerHTML={{ __html: currentLyric?.content?.rendered }}></div>
     </div>
